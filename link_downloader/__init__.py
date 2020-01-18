ee6fb14669616cb1650c11b94cb0b34d6686220c
Irina Velikopolskaya <i.velikopolskaya@protonmail.com>
2020-01-19 00:24:22 +0300

    initial commit

---
+++ link_downloader/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Link Downloader."""
+
+__author__ = """Irina Velikopolskaya"""
+__email__ = 'i.velikopolskaya@protonmail.com'
+__version__ = '0.1.0'
